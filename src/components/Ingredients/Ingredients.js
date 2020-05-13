7cd36f18414a93ce408fb0c3277696420ae0b34d
Daichi Kudo <sakanactor624@gmail.com>
2020-05-13 23:55:20 +0900

    :sparkles: LoadingとError表示でUX向上！useRefは分からん...

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,34 +2,20 @@ import React, { useState, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(process.env.REACT_APP_FIREBASE_DATABASE)
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((resData) => {
-  //       const loadedIngredients = [];
-  //       for (let key in resData) {
-  //         loadedIngredients.push({
-  //           id: key,
-  //           title: resData[key].title,
-  //           amount: resData[key].amount,
-  //         });
-  //       }
-  //       setIngredients(loadedIngredients);
-  //     });
-  // }, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setIngredients(filteredIngredients);
   }, []);
 
   const addIngredientsHandler = (ingredient) => {
+    setIsLoading(true);
     fetch(process.env.REACT_APP_FIREBASE_DATABASE, {
       method: "POST",
       body: JSON.stringify(ingredient),
@@ -39,6 +25,7 @@ const Ingredients = () => {
         return res.json();
       })
       .then((resData) => {
+        setIsLoading(false);
         setIngredients((prevIngredients) => [
           ...prevIngredients,
           { id: resData.name, ...ingredient },
@@ -47,14 +34,30 @@ const Ingredients = () => {
   };
 
   const removeIngredientsHandler = (ingredientId) => {
-    setIngredients((prevIngredients) =>
-      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-    );
+    setIsLoading(true);
+    fetch(`MOCK_DATABASE_URL`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        setIsLoading(false);
+        setIngredients((prevIngredients) =>
+          prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+        );
+      })
+      .catch((err) => {
+        setError("Something went wrong!");
+        setIsLoading(false);
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientsHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientsHandler} loading={isLoading} />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientsHandler} />
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,32 +6,45 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [filter, setFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = filter.length === 0 ? "" : `?orderBy="title"&equalTo="${filter}"`;
-    fetch(process.env.REACT_APP_FIREBASE_DATABASE + query)
-      .then((res) => {
-        return res.json();
-      })
-      .then((resData) => {
-        const loadedIngredients = [];
-        for (let key in resData) {
-          loadedIngredients.push({
-            id: key,
-            title: resData[key].title,
-            amount: resData[key].amount,
+    const timer = setTimeout(() => {
+      if (filter === inputRef.current.value) {
+        const query = filter.length === 0 ? "" : `?orderBy="title"&equalTo="${filter}"`;
+        fetch(process.env.REACT_APP_FIREBASE_DATABASE + query)
+          .then((res) => {
+            return res.json();
+          })
+          .then((resData) => {
+            const loadedIngredients = [];
+            for (let key in resData) {
+              loadedIngredients.push({
+                id: key,
+                title: resData[key].title,
+                amount: resData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [filter, onLoadIngredients]);
+      }
+    }, 100);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+          <input
+            ref={inputRef}
+            type="text"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
         </div>
       </Card>
     </section>
